d4c89f805088729f70e07b21911bab284e5bc15a
Tomi <tomasCduque@gmail.com>
2025-06-15 15:46:55 -0500

    Add documentation to script.js and changes

--- Modules/script.js
+++ Modules/script.js
@@ -4,6 +4,7 @@
 /*
     TO-DO:
         -Crear el ordenamiento por estatus.
+        -Documentar todo el código.
         -Agregar estilos iniciales a la pagina.
 
 */
@@ -17,32 +18,37 @@ const titleSort = document.getElementById("title-sort")
 const statusSort = document.getElementById("status-sort")
 const sortSection = document.getElementById("sort-section")
 const screen = document.getElementById("screen")
-let tasks = []
+const modal = document.getElementById("edit-modal")
 
-// Variables
+
+// Variable para almacenar la información de la tarea.
 let taskData = {
     title: "",
     description: ""
 }
 
-// Función para redirigir los datos del formulario para crear una tarea
+// Variable que asigna una id única a cada tarea nueva.
+let taskId = 0
+
+// Función para añadir al html la tarea con los datos del formulario.
 taskForm.addEventListener("submit", (event) => {
+    // Se evita que el formulario recargue la página.
     event.preventDefault()
     
-
+    // Se asigna a las propiedades del objeto taskData los valores dados por el formulario.
     taskData.title = document.getElementById("task-title").value
     taskData.description = document.getElementById("task-description").value
     
+    // Se llama la función createTask con taskData como argumento y se añade al html.
     screen.innerHTML += createTask(taskData)
+
+    // Se reinician los valores de los campos del formulario.
+    document.getElementById("task-description").value = ""
+    document.getElementById("task-title").value = ""
 })
 
-// Función para redirigir los datos del formulario de ordenamiento por status
-sortSection.addEventListener("submit", (event) => {
-    event.preventDefault()
-})
 
-
-// EVENTOS
+// Evento para manejar el display del formulario y el texto de los botones para añadir tareas.
 taskButton.addEventListener("click", ()=>{
     taskForm.classList.toggle("hidden")
     if(taskButton.innerText === "Add Task"){
@@ -51,33 +57,28 @@ taskButton.addEventListener("click", ()=>{
         taskButton.innerText = "Add Task"
     }
 })
-sortButton.addEventListener("click", ()=>{
-    sortSection.classList.toggle("hidden")
-    if(sortButton.innerText === "Sort"){
-        sortButton.innerText = "Exit"
-    } else {
-        sortButton.innerText = "Sort"
-    }
-})
 
 
 
+//<---!--->
 /*
-    Modulo encargado de administrar las tareas
-
-    TO-DO:
-
+    Modulo encargado de realizar las acciones de creación, eliminación y edición de
+    las tareas.
 */
-let taskClass = 0
+
+//  Función para crear tareas
 function createTask(task_data) {
-    taskClass += 1
+    taskId += 1
+
+    // Con la información de taskData crea una nueva tarea y retorna el html de la tarea.
+    // Al crear la tarea se asignan eventos click que llaman a las otras funciones con el objeto evento.
     return `
-            <div class="task" id="t${taskClass}">
+            <div class="task" id="t${taskId}">
                 <h3>${taskData.title}</h3>
                 <p>${taskData.description}</p>
                 <div class="task-actions">
-                    <button onclick="editTask(event)" value="t${taskClass}">edit</button>
-                    <button onclick="deleteTask(event)" value="t${taskClass}">delete</button>
+                    <button onclick="editTask(event)" value="t${taskId}">edit</button>
+                    <button onclick="deleteTask(event)" value="t${taskId}">delete</button>
                     <select name="status" class="task-status">
                         <option value="0">status</option>
                         <option value="1">pending</option>
@@ -88,47 +89,68 @@ function createTask(task_data) {
     `
 }
 
+// Función para eliminar una tarea.
 function deleteTask(e) {
+    // Busca el elemento que su id corresponde al valor del botón delete y lo elimina del HTML.
     const element = document.getElementById(`${e.target.value}`)
     element.remove()
 }
 
+// Función para editar una tarea.
 function editTask(e) {
+    /*
+        Se Almacena el elemento del botón que llamó al evento y se asigna a los botones del formulario de edición el valor del botón que llamo al evento.
+    */
     let element = document.getElementById(`${e.target.value}`)
-    let modal = document.getElementById("edit-modal")
     let buttons = [...modal.querySelectorAll("button")]
     buttons.forEach((button) => {
         button.value = e.target.value
     })
 
+    /*
+        Se elimina o añade la clase "hidden" al formulario, se asigna a los campos del formulario el titulo y la descripción del elemento que llamó al evento.
+    */
     modal.classList.toggle("hidden")
     modal.querySelector("#edit-title").value = element.querySelector("h3").innerText
     modal.querySelector("#edit-description").value = element.querySelector("p").innerText
 }
 
+// Funciones del formulario de edición.
+// Función que modifica los campos de la tarea donde se llamó el evento editTask. 
 function acceptChanges(e) {
+    // Se llama y almacena al elemento con la id igual al valor del botón donde fué llamado el evento.
     let element = document.getElementById(`${e.target.value}`)
-    let modal = document.getElementById("edit-modal")
 
     modal.classList.toggle("hidden")
     element.querySelector("h3").innerText = modal.querySelector("#edit-title").value
     element.querySelector("p").innerText = modal.querySelector("#edit-description").value
-
-    console.log(element)
 }
 
+// Función que esconde el formulario y vacía sus campos.
 function cancelChanges(e) {
-    let modal = document.getElementById("edit-modal")
     modal.querySelector("#edit-title").value = ""
     modal.querySelector("#edit-description").value = ""
     modal.classList.toggle("hidden")
-
 }
 
 
+
+//<---!--->
 /*
-    Módulo encargado del ordenamiento de las tareas
-
-    TO-DO:
-
+    Módulo encargado del ordenamiento de las tareas por status, titulo y alfabeticamente.
 */
+
+// Función para redirigir los datos del formulario de ordenamiento por status
+sortSection.addEventListener("submit", (event) => {
+    event.preventDefault()
+})
+
+// Evento para manejar el display de la sección sort y el texto del botón.
+sortButton.addEventListener("click", ()=>{
+    sortSection.classList.toggle("hidden")
+    if(sortButton.innerText === "Sort"){
+        sortButton.innerText = "Exit"
+    } else {
+        sortButton.innerText = "Sort"
+    }
+})
